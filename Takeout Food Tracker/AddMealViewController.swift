2acf2661882b872387c9caeff0c3efa41b6888c1
George Ashton <georgeashton95@gmail.com>
2020-01-03 14:16:26 +0000

    Cancel button logic

--- Takeout Food Tracker/AddMealViewController.swift
+++ Takeout Food Tracker/AddMealViewController.swift
@@ -115,6 +115,13 @@ class AddMealViewController: UIViewController, UITextFieldDelegate, UIImagePicke
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     */
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
