675f02f1d632e37782e49bd639e47a81ed4fcb7a
George Ashton <georgeashton95@gmail.com>
2020-01-03 15:16:09 +0000

    Delete logic

--- Takeout Food Tracker/AddMealViewController.swift
+++ Takeout Food Tracker/AddMealViewController.swift
@@ -125,7 +125,21 @@ class AddMealViewController: UIViewController, UITextFieldDelegate, UIImagePicke
     */
     
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+
+
     }
     
     
